f593257f6fd0a74f09c96ba8e3cc2ad659e70e51
jaepa1212 <jaepatv@naver.com>
2023-04-04 11:22:30 +0900

    11:22

--- good mon.py
+++ good mon.py
@@ -1,1 +1,15 @@
 print(3*5)
+print('Mary\'s cosmetics')
+print("안녕하세요.\n만나서\t반갑습니다.")
+s = "hello"
+t = "python"
+print(s,"!", t) 
+print(t)
+A = "good"
+B = "morn"
+print(A,B+"!", sep="/")
+a = 128
+print(type(a))
+print(type(A))
+a = "132"
+print(type(a))
